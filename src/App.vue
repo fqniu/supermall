af579ff3c82c0efb339e8c23f0abb98df6fa29c7
fqniu <1966798438@qq.com>
2020-08-31 23:05:05 +0800

    tabbar的划分

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <tab-bar>
+      <tab-bar-item>
+        <template v-slot:item-icon>
+          <img src="./assets/img/tabbar/home.svg" alt />
+        </template>
+        <template v-slot:item-icon-active>
+          <img src="./assets/img/tabbar/home_active.svg" alt />
+        </template>
+        <template v-slot:item-text>
+          <div>首页</div>
+        </template>
+      </tab-bar-item>
+      <tab-bar-item>
+        <template v-slot:item-icon>
+          <img src="./assets/img/tabbar/category.svg" alt />
+        </template>
+          <template v-slot:item-icon-active>
+          <img src="./assets/img/tabbar/category_active.svg" alt />
+        </template>
+        <template v-slot:item-text>
+          <div>分类</div>
+        </template>
+      </tab-bar-item>
+      <tab-bar-item>
+        <template v-slot:item-icon>
+          <img src="./assets/img/tabbar/cart.svg" alt />
+        </template>
+          <template v-slot:item-icon-active>
+          <img src="./assets/img/tabbar/cart_active.svg" alt />
+        </template>
+        <template v-slot:item-text>
+          <div>购物车</div>
+        </template>
+      </tab-bar-item>
+      <tab-bar-item>
+        <template v-slot:item-icon>
+          <img src="./assets/img/tabbar/profile.svg" alt />
+        </template>
+          <template v-slot:item-icon-active>
+          <img src="./assets/img/tabbar/profile_active.svg" alt />
+        </template>
+        <template v-slot:item-text>
+          <div>我的</div>
+        </template>
+      </tab-bar-item>
+    </tab-bar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TabBar from "./components/tabbar/TabBar.vue";
+import TabBarItem from "./components/tabbar/TabBarItem.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TabBar,
+    TabBarItem,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/css/base.css";
 </style>
---
+++ src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="tab-bar-item">
+    <!-- 注意插槽会被切换掉的 也就是说样式会被替换掉 可以给slot再套一个标签-->
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+    <div :class="{active:isActive}">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isActive: true,
+    };
+  },
+};
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  /* 去掉图片间隙 */
+  vertical-align: middle;
+}
+.active {
+  color: red;
+}
+</style>
